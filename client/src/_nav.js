30aeec69718ad1f07a1a5bc5ee9ebfa1a7e0ec8a
Amol Bhasme <amolbh@vyom-labs.com>
2020-02-04 11:40:02 +0530

    New Structure UI

---
+++ client/src/_nav.js
@@ -0,0 +1,22 @@
+export default {
+  items: [
+    {
+      name: 'Dashboard',
+      url: '/dashboard',
+      icon: 'icon-speedometer',
+      badge: {
+        variant: 'info',
+      },
+    
+    },
+    {
+      name: 'Audit Plan',
+      // url: '/RasaPredictor',
+      icon: 'icon-disc',
+      badge: {
+        variant: 'info',
+      },
+    },  
+  
+  ]
+};
